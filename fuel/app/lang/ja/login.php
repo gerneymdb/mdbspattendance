190ad08bcfeeb84a03479922c0e94f615d7dcd89
Edmund Arias <ariasedmundmoses@gmail.com>
2018-01-25 17:58:06 +0800

    Translation of japanese attendance.php and login.php

--- fuel/app/lang/ja/login.php
+++ fuel/app/lang/ja/login.php
@@ -1,52 +1,52 @@
 <?php
     return array(
-        "title" => "Login",
+        "title" => "????",
 
-        "token" => "Token missing or not valid.",
+        "token" => "??????????????",
 
         "lang_form" => array(
 
-            "select_lang"   => "Select Language",
-            "translate"     => "Translate",
+            "select_lang"   => "?????",
+            "translate"     => "??",
         ),
 
         "login_form" => array(
             "id_placeholder"        => "ID",
-            "password_placeholder"  => "password",
-            "submit_btn"            => "Submit",
-            "form_heading"          => "Login",
+            "password_placeholder"  => "?????",
+            "submit_btn"            => "??",
+            "form_heading"          => "????",
 
             "form_error" => array(
-                "empty_field"   => "Please fill out the fields.",
-                "incorrect"     => "Incorrect userid or password",
-                "many_attempt"  => "Too many login attempt. Try again later after."
+                "empty_field"   => "???????????????",
+                "incorrect"     => "???????ID????????",
+                "many_attempt"  => "?????????????????????????"
             )
 
         ),
 
         "first_login_form" => array(
-            "old_pwd"       => "old Password",
-            "new_pwd"       => "new password",
-            "confirm_pwd"   => "confirm password",
-            "save_btn"      => "save",
-            "form_heading"  => "First time login",
-            "success"       => "New password saved",
+            "old_pwd"       => "????????",
+            "new_pwd"       => "????????",
+            "confirm_pwd"   => "??????????",
+            "save_btn"      => "???",
+            "form_heading"  => "????????",
+            "success"       => "????????????????",
 
             "form_error" => array(
-                "incorrect_old_pwd" => "Old password is incorrect"
+                "incorrect_old_pwd" => "???????????????"
             )
         ),
 
         "scheduled_pwd_reset" => array(
-            "old_pwd"       => "old Password",
-            "new_pwd"       => "new password",
-            "confirm_pwd"   => "confirm password",
-            "save_btn"      => "save",
-            "form_heading"  => "Your current password has already expired. Change your password",
-            "success"       => "New password saved",
+            "old_pwd"       => "????????",
+            "new_pwd"       => "????????",
+            "confirm_pwd"   => "??????????",
+            "save_btn"      => "???",
+            "form_heading"  => "?????????????????????????????????",
+            "success"       => "????????????????",
 
             "form_error" => array(
-                "incorrect_old_pwd" => "Old password is incorrect"
+                "incorrect_old_pwd" => "???????????????"
             )
         )
 
