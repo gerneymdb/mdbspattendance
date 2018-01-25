0a721ce1e93d47ddc14791f4c2b6aca7cdd14525
sherm <shermaine@gmail.com>
2018-01-25 15:51:59 +0800

    done change the text of attendance and login

--- fuel/app/lang/ta/login.php
+++ fuel/app/lang/ta/login.php
@@ -1,52 +1,52 @@
 <?php
     return array(
-        "title" => "Login",
+        "title" => "Mag log in",
 
-        "token" => "Token missing or not valid.",
+        "token" => "Nawawala o hindi wasto ang token.",
 
         "lang_form" => array(
 
-            "select_lang"   => "Select Language",
-            "translate"     => "Translate",
+            "select_lang"   => "Piliin ang Wika",
+            "translate"     => "Isalin",
         ),
 
         "login_form" => array(
             "id_placeholder"        => "ID",
             "password_placeholder"  => "password",
-            "submit_btn"            => "Submit",
-            "form_heading"          => "Login",
+            "submit_btn"            => "Ipasa",
+            "form_heading"          => "Mag log in",
 
             "form_error" => array(
-                "empty_field"   => "Please fill out the fields.",
-                "incorrect"     => "Incorrect userid or password",
-                "many_attempt"  => "Too many login attempt. Try again later after."
+                "empty_field"   => "Mangyaring punan ang mga patlang.",
+                "incorrect"     => "Maling userid o password",
+                "many_attempt"  => "Masyadong maraming pagtatangka sa pag-login. Subukan ulit mamaya pagkatapos."
             )
 
         ),
 
         "first_login_form" => array(
-            "old_pwd"       => "old Password",
-            "new_pwd"       => "new password",
-            "confirm_pwd"   => "confirm password",
-            "save_btn"      => "save",
-            "form_heading"  => "First time login",
-            "success"       => "New password saved",
+            "old_pwd"       => "Lumang password",
+            "new_pwd"       => "bagong password",
+            "confirm_pwd"   => "kumpirmahin ang password",
+            "save_btn"      => "i-save",
+            "form_heading"  => "Unang beses na pag-login",
+            "success"       => "Nai-save na bagong password",
 
             "form_error" => array(
-                "incorrect_old_pwd" => "Old password is incorrect"
+                "incorrect_old_pwd" => "Ang maling password ay hindi tama"
             )
         ),
 
         "scheduled_pwd_reset" => array(
-            "old_pwd"       => "old Password",
-            "new_pwd"       => "new password",
-            "confirm_pwd"   => "confirm password",
-            "save_btn"      => "save",
-            "form_heading"  => "Your current password has already expired. Change your password",
-            "success"       => "New password saved",
+            "old_pwd"       => "Lumang password",
+            "new_pwd"       => "bagong password",
+            "confirm_pwd"   => "kumpirmahin ang password",
+            "save_btn"      => "i-save",
+            "form_heading"  => "kumpirmahin na ang iyong kasalukuyang password ay nag-expire na. Baguhin ang iyong password",
+            "success"       => "Nai-save na bagong password",
 
             "form_error" => array(
-                "incorrect_old_pwd" => "Old password is incorrect"
+                "incorrect_old_pwd" => "Ang maling password ay hindi tama"
             )
         )
 
