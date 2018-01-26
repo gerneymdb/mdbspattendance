e33b95b8f4616fc6d734852970d3fab3f499599f
Edmund Arias <ariasedmundmoses@gmail.com>
2018-01-26 10:09:50 +0800

    Translation of japanese attendance.php and login.php final changes

--- fuel/app/lang/ja/login.php
+++ fuel/app/lang/ja/login.php
@@ -1,52 +1,52 @@
 <?php
     return array(
-        "title" => "????",
+        "title" => "ログイン",
 
-        "token" => "??????????????",
+        "token" => "トークンがないか、無効です。",
 
         "lang_form" => array(
 
-            "select_lang"   => "?????",
-            "translate"     => "??",
+            "select_lang"   => "言語を選択",
+            "translate"     => "翻訳",
         ),
 
         "login_form" => array(
             "id_placeholder"        => "ID",
-            "password_placeholder"  => "?????",
-            "submit_btn"            => "??",
-            "form_heading"          => "????",
+            "password_placeholder"  => "パスワード",
+            "submit_btn"            => "送信",
+            "form_heading"          => "ログイン",
 
             "form_error" => array(
-                "empty_field"   => "???????????????",
-                "incorrect"     => "???????ID????????",
-                "many_attempt"  => "?????????????????????????"
+                "empty_field"   => "フィールドに記入してください。",
+                "incorrect"     => "不正なユーザーIDまたはパスワード",
+                "many_attempt"  => "ログイン回数が多すぎます。後でやり直してください。"
             )
 
         ),
 
         "first_login_form" => array(
-            "old_pwd"       => "????????",
-            "new_pwd"       => "????????",
-            "confirm_pwd"   => "??????????",
-            "save_btn"      => "???",
-            "form_heading"  => "????????",
-            "success"       => "????????????????",
+            "old_pwd"       => "古いのパスワード",
+            "new_pwd"       => "新しいパスワード",
+            "confirm_pwd"   => "パスワードを認証する",
+            "save_btn"      => "セーブ",
+            "form_heading"  => "初めてのログイン",
+            "success"       => "新しいパスワードが保存されました",
 
             "form_error" => array(
-                "incorrect_old_pwd" => "???????????????"
+                "incorrect_old_pwd" => "古いパスワードが間違っています"
             )
         ),
 
         "scheduled_pwd_reset" => array(
-            "old_pwd"       => "????????",
-            "new_pwd"       => "????????",
-            "confirm_pwd"   => "??????????",
-            "save_btn"      => "???",
-            "form_heading"  => "?????????????????????????????????",
-            "success"       => "????????????????",
+            "old_pwd"       => "古いのパスワード",
+            "new_pwd"       => "新しいパスワード",
+            "confirm_pwd"   => "パスワードを認証する",
+            "save_btn"      => "セーブ",
+            "form_heading"  => "現在のパスワードはすでに期限切れです。パスワードを変更してください",
+            "success"       => "新しいパスワードが保存されました",
 
             "form_error" => array(
-                "incorrect_old_pwd" => "???????????????"
+                "incorrect_old_pwd" => "古いパスワードが間違っています"
             )
         )
 
