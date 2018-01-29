aad8eb8f4bc0963568c6280495abc7878e907a0a
cheng06 <cheryl@gmail.com>
2018-01-29 14:15:53 +0800

    translation

--- fuel/app/lang/en/new_administrator.php
+++ fuel/app/lang/en/new_administrator.php
@@ -1,4 +1,46 @@
 <?php
 return array(
+    //words
+    "Add" => "add",
+    "A" => "a",
+    "New" => "new",
+    "Administrator" => "administrator",
+    "User"  => "user",
+    "Name" => "name",
+    "Email" => "email",
+    "Address" => "address",
+    "First" => "first",
+    "Middle" => "middle",
+    "The" => "the",
+    "Field" =>"field",
+    "Is" => "is",
+    "Require" => "require",
+    "Required" => "required",
+    "And" => "and",
+    "Must" => "must",
+    "Contain" => "contain",
+    "Value" => "value",
+    "Admin" => "Admin",
+    "Successfully" => "successfully",
+    "Success" => "success",
+    "Added" => "added",
+
+
+    //phrases
+    "Add a new administrator" => "add a new administrator",
+    "Administrator's username" => "administrator's username",
+    "Administrator's email" => "administrator's email",
+    "Admin's first name" => "admin's first name",
+    "Admin's middle name" => "admin's middle name",
+    "admin's last name" => "admin's last name",
+    "The field First Name is required and must contain a value." => "the field first name is required and must contain a value.",
+    "The field Last Name is required and must contain a value." => "the field last name is required and must contain a value.",
+    "The field Username is required and must contain a value." => "the field username is required and must contain a value.",
+    "The field Email Address Name is required and must contain a value." => "the email address name is required and must contain a value.",
+    "New administrator is successfully added." => "new administrator is successfully added.",
+
+
+
+
 
 );
