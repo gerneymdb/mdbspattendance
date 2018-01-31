01a0d45df17691c0411de97bd88293d37724a265
sherm <shermaine@gmail.com>
2018-01-31 17:07:10 +0800

    done translate manageemployee and new administration.

---
+++ fuel/app/lang/ta/new_administrator.php
@@ -0,0 +1,51 @@
+<?php
+return array(
+    //words
+    "Add" => "idagdag",
+    "New" => "bago",
+    "Administrator" => "administrator",
+    "User"  => "user",
+    "Name" => "pangalan",
+    "Email" => "email",
+    "Address" => "address",
+    "First" => "una",
+    "Middle" => "gitna",
+    "The" => "ang",
+    "Field" =>"patlang",
+    "Is" => "ay",
+    "Require" => "nangangailangan",
+    "Required" => "kailangan",
+    "And" => "at",
+    "Must" => "dapat",
+    "Contain" => "naglalaman ng",
+    "Value" => "halaga",
+    "Admin" => "Admin",
+    "Successfully" => "matagumpay",
+    "Success" => "tagumpay",
+    "Added" => "idinagdag",
+    "Already" => "handa",
+    "exist" => "umiiral",
+    "exists" =>"umiiral",
+
+
+    //phrases
+    "Add a new administrator" => "magdagdag ng isang bagong administrator",
+    "Administrator's username" => "username ng administrator",
+    "Administrator's email" => "Email ng Administrator",
+    "Admin's first name" => "unang pangalan ng admin",
+    "Admin's middle name" => "Ang gitnang pangalan ng admin",
+    "admin's last name" => "apelyido ng admin",
+    "The field First Name is required and must contain a value." => "kinakailangan ang unang pangalan ng patlang at dapat maglaman ng isang halaga.",
+    "The field Last Name is required and must contain a value." => "ang huling pangalan ng field ay kinakailangan at dapat maglaman ng isang halaga.",
+    "The field Username is required and must contain a value." => "ang patlang na username ay kinakailangan at dapat maglaman ng isang halaga.",
+    "The field Email Address Name is required and must contain a value." => "ang pangalan ng email address ay kinakailangan at dapat maglaman ng isang halaga.",
+    "New administrator is successfully added." => "Matagumpay na naidagdag ang bagong administrator.",
+    "Email address already exists." => "umiiral ang email address.",
+    "New admin successfully added"  => "Matagumpay na idinagdag ang bagong admin",
+    "Something went wrong in adding new administrator!" => "May naging mali sa pagdaragdag ng bagong administrator!"
+
+
+
+
+
+);
