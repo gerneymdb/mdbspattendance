ad6471b29546fc5fffb117e024f2a4342d64a2a8
maricel <maricel.gmail.com>
2018-01-31 16:48:42 +0800

    translation languange in ilonggo changes..

--- fuel/app/lang/ilo/admin_manageattendance.php
+++ fuel/app/lang/ilo/admin_manageattendance.php
@@ -70,6 +70,6 @@ return array(
     "this month's attendance"      => "presensya sang sini nga bulan",
     "daily attendance count"       => "kada-adlaw nga pag-isip sang presensya ",
     "attendance information updated" => "pag update sang presensya nga inpormation",
-    "illegal operation. missing token. hit refresh" => "illegal nga operation. kg pagkadula sang token. tum.okon ang refresh",
+    "illegal operation. missing token. hit refresh" => "illegal nga operation. kg pagkadula sang token. tum-okon ang refresh",
 
 );
