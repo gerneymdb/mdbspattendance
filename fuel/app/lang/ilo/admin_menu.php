ef6e6fbd7e94b41311f9bb6362abb41a14963bc9
gerney <gerncnalda@gmail.com>
2018-02-01 10:00:59 +0800

    =ilonggo translation of admin menu

---
+++ fuel/app/lang/ilo/admin_menu.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+    "manage attendance"     =>  "pagdumala sang attendance",
+    "manage employee"       =>  "manage empleyado",
+    "holidays"              =>  "holidays",
+    "leave applications"    =>  "mga applikasyon sang leave",
+    "work schedule"         =>  "skedyul sang ubra",
+    "system settings"       => "sistema sang setting",
+    "new administrator"     => "bag-o nga manugdumala"
+);
