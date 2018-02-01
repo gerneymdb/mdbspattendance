12917a4b68dbe21262b35a1a4a131c1576958237
gerney <gerncnalda@gmail.com>
2018-02-01 10:32:01 +0800

    =admin menu translation tagalog and japanese

---
+++ fuel/app/lang/ta/admin_menu.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+    "manage attendance"     =>  "pamahalaan ang pagdalo",
+    "manage employee"       =>  "pamahalaan ang empleyado",
+    "holidays"              =>  "mga holiday",
+    "leave applications"    =>  "mga application nang leave",
+    "work schedule"         =>  "iskedyul nang trabaho",
+    "system settings"       => "setting nang sistema",
+    "new administrator"     => "bagong tagapamahala"
+);
